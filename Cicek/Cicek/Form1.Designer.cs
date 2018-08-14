8562fc119c7d75ae5c2d4de2f7206ed3444a6098
ONUR BALKANLI <onur.balkanli@hotmail.com>
2018-08-14 15:24:12 +0300

    Add files via upload

---
+++ Cicek/Cicek/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Cicek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblTur = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 261);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(472, 192);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cicek.Properties.Resources.cicek1;
+            this.pictureBox1.Location = new System.Drawing.Point(233, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 135);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(68, 81);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "Baslat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "isim",
+            "sehir",
+            "hayvan"});
+            this.comboBox1.Location = new System.Drawing.Point(82, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // lblTur
+            // 
+            this.lblTur.AutoSize = true;
+            this.lblTur.Location = new System.Drawing.Point(26, 37);
+            this.lblTur.Name = "lblTur";
+            this.lblTur.Size = new System.Drawing.Size(35, 13);
+            this.lblTur.TabIndex = 5;
+            this.lblTur.Text = "Tür :>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(500, 465);
+            this.Controls.Add(this.lblTur);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ÇiÇEK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblTur;
+    }
+}
+
---
+++ Cicek/Cicek/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cicek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button[] btn;
+        bool sifir = false;
+        int harf = 65;
+        int r=1,k=0,t=0,sayac=0;
+        string[] kelimeler;
+        string kelime;
+        Label[] lDizi;
+        Label[] lDizi2;
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void kelimesec()
+        {
+            kelime = kelimeler[rnd.Next(0, 4)];
+            
+            lDizi = new Label[kelime.Length];
+            lDizi2 = new Label[kelime.Length];
+            for (int i = 0; i < kelime.Length; i++)
+            {           
+                lDizi[i] = new Label();
+                lDizi2[i] = new Label();
+                
+                panel2.Controls.Add(lDizi[i]);
+                lDizi[i].BorderStyle = BorderStyle.Fixed3D;
+                lDizi[i].Location = new Point(25 * i, 55);
+                lDizi[i].Width = 25;
+                lDizi[i].Text = "_";
+                lDizi2[i].Text = kelime.Substring(0 + i, 1);
+                
+                lDizi[i].Visible = true;
+                lDizi2[i].Visible = false;
+            }
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex==0)
+            {
+                kelimeler = new string[] { "onur", "dogan", "ali", "veli" };
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                kelimeler = new string[] { "istanbul", "izmir", "adana", "ankara" };
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                kelimeler = new string[] { "kedi", "kopek", "balik", "kus" };
+            }
+
+            if (sifir==true)
+            {
+                sifirla();
+            }
+            
+            Harfler();
+            kelimesec();
+        }
+
+        private void sifirla()
+        {
+            harf = 65;
+            r = 1; k = 0; t = 0; sayac = 0;
+            pictureBox1.Image = Properties.Resources.cicek1;
+            for (int i = 0; i < 27; i++)
+            {
+                panel1.Controls.Remove(btn[i]);
+            }
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                panel2.Controls.Remove(lDizi[i]);
+            }
+            
+        }
+
+        /// <summary>
+        /// Harflerin Olustugu yer.
+        /// </summary>
+        private void Harfler()
+        {
+            sifir = true;
+            btn = new Button[27];
+           
+            for (int i = 0; i < 3; i++)
+            {
+                for (int s = 0; s < 9; s++)
+                {
+                    
+                    
+                    
+                    btn[k] = new Button();
+                    
+                    btn[k].Text = ((char)harf).ToString();
+                    
+                    btn[k].Size = new Size(50, 50);
+                    btn[k].Location = new Point(52 * s, 50 * i);
+                    panel1.Controls.Add(btn[k]);
+                    harf = harf + 1;
+                    btn[k].Click += Btn_Click;
+                    btn[k].Visible = true;
+                    if (k==26)
+                    {
+                        btn[k].Text = "İPUCU";
+                        break;
+                    }
+                    k++;
+                }
+            }
+            
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            
+            bool d = false;
+            sayac = sayac + 1;
+            Button sbuton = (sender as Button);
+            
+            if (sbuton.Text== "İPUCU")
+            {
+                
+                for (int i = 0; i < kelime.Length; i++)
+                {
+                    
+
+                    if (lDizi[i].Text=="_")
+                    {
+                        sbuton.Text = lDizi2[i].Text;
+                        break;
+                    }
+                }
+                
+                
+            }
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                
+                if (lDizi2[i].Text==sbuton.Text.ToLower())
+                {
+                    lDizi2[i].Visible = false;
+                    lDizi[i].Text = lDizi2[i].Text;
+                    d = true;
+                    t = t + 1;
+                }
+                
+                else if (sbuton.Text.ToLower()=="ı" && lDizi2[i].Text=="i")
+                {
+                    lDizi2[i].Visible = false;
+                    lDizi[i].Text = lDizi2[i].Text.ToUpper();
+                    d = true;
+                    t = t + 1;
+                }
+                else if (sbuton.Text.ToLower() == "u" && lDizi2[i].Text == "ü")
+                {
+                    lDizi2[i].Visible = false;
+                    lDizi[i].Text = lDizi2[i].Text;
+                    d = true;
+                    t = t + 1;
+                }
+                else if (sbuton.Text.ToLower() == "o" && lDizi2[i].Text == "ö")
+                {
+                    lDizi2[i].Visible = false;
+                    lDizi[i].Text = lDizi2[i].Text;
+                    d = true;
+                    t = t + 1;
+                }
+                else if (sbuton.Text.ToLower() == "c" && lDizi2[i].Text == "ç")
+                {
+                    lDizi2[i].Visible = false;
+                    lDizi[i].Text = lDizi2[i].Text;
+                    d = true;
+                    t = t + 1;
+                }
+                
+                 if (t==kelime.Length)
+                {
+                    MessageBox.Show(sayac + " denemede bildiniz =)","Tebrikler" );
+                    for (int k = 0; k < 27; k++)
+                    {
+                        btn[k].Enabled = false;
+                    }
+                    break;
+                    
+
+                }
+                else if (i + 1 == kelime.Length && d == false)
+                {
+                    r = r + 1;
+                    resim();
+                    break;
+
+                }
+
+            }
+            sbuton.Enabled = false;
+        }
+
+        private void resim()
+        {
+            switch (r)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.cicek1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.cicek2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.cicek3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.cicek4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.cicek5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.cicek6;
+                    MessageBox.Show("Tekrar Dene","GameOver");
+                    for (int k = 0; k < 27; k++)
+                    {
+                        btn[k].Enabled = false;
+                    }
+                    break;
+                
+            }
+            
+        }
+
+        
+    }
+}
